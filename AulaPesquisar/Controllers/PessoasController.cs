c056928b7a15555c2f53297874e8b1826cdfe74a
MARIA ARRUDA <maria.arruda@fatec.sp.gov.br>
2022-03-25 21:54:50 -0300

    aula dia 25-03

--- AulaPesquisar/Controllers/PessoasController.cs
+++ AulaPesquisar/Controllers/PessoasController.cs
@@ -12,9 +12,32 @@ namespace AulaPesquisar.Controllers
             db = _db;   
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string query, string itembusca)
         {
-            return View();
+            if (string.IsNullOrEmpty(query))
+            {
+                return View(db.PESSOAS.ToList());
+            }
+            else
+            {
+                switch (itembusca)
+                {
+                    default:
+                        return View(db.PESSOAS.Where(a => a.nome.Contains(query) || a.cpf.Contains(query) || a.rg.Contains(query) || a.idade.Contains(query)));
+
+                    case "Idade":
+                        return View(db.PESSOAS.Where(a => a.idade == query));
+                    case "CPF":
+                        return View(db.PESSOAS.Where(a => a.cpf == query));
+                    case "RG":
+                        return View(db.PESSOAS.Where(a => a.rg == query));
+                    case "Nome":
+                        return View(db.PESSOAS.Where(a => a.nome.Contains (query)));
+                       
+                }
+            }
+               
+            
         }
     }
 }
